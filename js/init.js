942e764a9245810b5937d018897ccc5302354e35
manuel3cv51 <94109277+manuel3cv51@users.noreply.github.com>
2021-11-27 20:38:14 -0600

    Subida de archivos

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyD_w7tElrRY_w11qeihIEh5qmQ0uAvYvhc",
+  authDomain: "fbautenz.firebaseapp.com",
+  projectId: "fbautenz",
+  storageBucket: "fbautenz.appspot.com",
+  messagingSenderId: "71744563029",
+  appId: "1:71744563029:web:2ffbf59088bb7f9a30f1dc",
+  measurementId: "G-7H71EFJ64N"
+});
